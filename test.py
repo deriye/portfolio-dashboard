97e081b7fd49dc55c0e2e0eb926ec38a792f6989
deriye <omarderiye@gmail.com>
2021-07-01 18:28:48 +0200

    updated test.py

--- test.py
+++ test.py
@@ -26,26 +26,36 @@ def percentage_change(buy_price, sell_price):
 while True:
     assets = []
 
-    option = input("Options (Add, Delete, List, Quit): ")
+    option = input("Options (List, Add, Delete, Quit): ")
 
     if (option == "Quit"):
         break
 
-    asset_name = input("Asset name: ") #input("Asset name: ")
-    closing_prices = web.DataReader(asset_name, data_source="yahoo", start="2019-01-01", end="2021-07-01")["Close"]#["2021-06-26"]
-    year = input("Year: ")
-    month = input("Month: ")
-    day = input("Day: ")
-    buy_date = year + "-" + month + "-" + day
-    amount_invested = int(input("Amount invested: "))
-    buy_price = closing_prices[buy_date]
-    today_date = "2021-07-01"
-    sell_price = closing_prices[today_date]
-    print("buy price:", buy_price)
-    print("sell price:", sell_price)
-    change = percentage_change(buy_price, sell_price)
-    portfolio_value = amount_invested * (1 + change)
-    print("amount invested:", amount_invested)
-    print("portfolio value:", portfolio_value)
-    print("profit:", portfolio_value - amount_invested, "("+ str(round(change * 100, 2)) +"%)")
-    print("")
+    elif (option == "List"):
+        print("List all assets order by percetange change")
+
+    elif (option == "Add"):
+        asset_name = input("Asset name: ") #input("Asset name: ")
+        closing_prices = web.DataReader(asset_name, data_source="yahoo", start="2019-01-01", end="2021-07-01")["Close"]#["2021-06-26"]
+        year = input("Year: ")
+        month = input("Month: ")
+        day = input("Day: ")
+        buy_date = year + "-" + month + "-" + day
+        amount_invested = int(input("Amount invested: "))
+        buy_price = closing_prices[buy_date]
+        today_date = "2021-07-01"
+        sell_price = closing_prices[today_date]
+        print("buy price:", buy_price)
+        print("sell price:", sell_price)
+        change = percentage_change(buy_price, sell_price)
+        portfolio_value = amount_invested * (1 + change)
+        print("amount invested:", amount_invested)
+        print("portfolio value:", portfolio_value)
+        print("profit:", portfolio_value - amount_invested, "("+ str(round(change * 100, 2)) +"%)")
+        print("")
+
+    elif (option == "Delete"):
+        print("Delete operation")
+
+    else:
+        print("Didn't understand input, try again!")
